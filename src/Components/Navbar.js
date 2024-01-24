ff89e0d374f699aa484d752de8f81b1029447030
usama508 <musama023418@gmail.com>
2024-01-24 17:16:17 +0500

    latest commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -33,9 +33,9 @@ function NavBar() {
   <>
     <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
       <Container>
-        <Navbar.Brand href="#home">
+        {/* <Navbar.Brand href="#home">
             <img src={logo} alt='Logo'/>
-        </Navbar.Brand>
+        </Navbar.Brand> */}
         <Navbar.Toggle aria-controls="basic-navbar-nav" >
             <span className='navbar-toggler-icon'></span>
         </Navbar.Toggle>
